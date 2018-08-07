cb175e1d4db07a69a9e53425b9b9d1962dfb580b
Roenna <mil.lukasik@gmail.com>
2018-08-07 20:00:10 +0200

    Add progress bar

--- app.js
+++ app.js
@@ -7,19 +7,28 @@ new Vue ({
             eventDesc: "<b>Finally!</b> We're going on our little trip in the mountains for a couple of days",
         },
         newNameText: '',
-        stalkersList: [],
-        formSubmitClass: "",
+        stalkersList: ['Milena'],
         appStyles: {
             marginTop: '25px',
-        }
+        },
+        eventCapacity: 10,
+        eventCapacityPercentage: 0
     },
     methods: {
         addStalker : function() {
-            if(this.newNameText.length > 0){
+            if(this.newNameText.length > 0 & this.stalkersList.length < this.eventCapacity){
                 this.stalkersList.push(this.newNameText);
                 this.newNameText = '';
-                this.formSubmitClass = "submitted"
+                this.eventCapacityPercentage = this.stalkersList.length/this.eventCapacity * 100;
             }
+        },
+        methodCall: function() {
+            this.appStyles.marginTop = "50px";
         }
     },
+    computed: {
+        sortNames: function() {
+            return this.stalkersList.sort();
+        }
+    }
 });
